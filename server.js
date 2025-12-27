aca2bbdf9fb025b70ce0e3e9e0c50fec4f404b56
Sayyad jani pasha <167852187+SayyadJani@users.noreply.github.com>
2025-12-28 02:03:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*", // TEMPORARY (for debugging)
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
